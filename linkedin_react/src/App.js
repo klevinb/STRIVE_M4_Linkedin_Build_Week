7f670ac0f2ce3a8bdfa23edd59916e77d6bdc6bf
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-06-15 15:27:03 +0200

    First commit

--- linkedin_react/src/App.js
+++ linkedin_react/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-
+<NavBar /> 
     </div>
   );
 }
