c8f0b9dc8eeaef6d1ce04e5058e6bb2ec38d2ff2
segundara <segundara@gmail.com>
2020-06-15 15:54:57 +0300

    another update to right_side_content

--- linkedin_react/src/components/SideContent.jsx
+++ linkedin_react/src/components/SideContent.jsx
@@ -35,49 +35,45 @@ class SideContent extends React.Component {
 
     render(){
         return (
-            <Container>
-                <Row >
-                    <Col md={4} className="offset-md-8 pt-5">
-                        <Row className="pb-5 d-flex flex-column ">
-                            <Col className="col pt-3 border-bottom d-flex">
-                                <p>Edit public profile and URL</p>
-                                <i className="fa fa-question-circle-o ml-auto"></i>
-                            </Col>
-                            <Col className="col pt-3 border-bottom d-flex">
-                                <p>Add profile in another language</p>
-                                <i className="fa fa-question-circle-o ml-auto"></i>
-                            </Col>
-                        </Row>
-                        <p>People also viewed</p>
-                        {this.state.users.map((user,i)=>{
-                            return (
-                                <Row className="pb-3" key={i}>
-                                    <Col md={4}>
-                                        <img
-                                            src='https://img.icons8.com/officel/2x/user.png'
-                                            style={{height:"5rem", width: "5rem"}}
-                                            className="card-img img-fluid"
-                                            alt="image"
-                                        />
-                                        
-                                    </Col>
-                                    <Col className="col col-8 d-flex pt-3 border-bottom">
-                                        <Row className="w-100">
-                                            <Col sm={10} className="d-flex flex-column w-75">
-                                            <strong>{user.name} {user.surname}</strong>
-                                            <span>{user.title}</span>
-                                            </Col>
-                                            <Col sm={2}>
-                                            <span><i className="fa fa-user-plus"></i></span>
-                                            </Col>
-                                        </Row>                                        
-                                    </Col>
-                                </Row>
-                            )
-                        })}
-                    </Col>
-                </Row>
-            </Container>
+                <Col md={4} className="offset-md-8 pt-5">
+                    <Row className="pb-5 d-flex flex-column ">
+                        <Col className="col pt-3 border-bottom d-flex">
+                            <p>Edit public profile and URL</p>
+                            <i className="fa fa-question-circle-o ml-auto"></i>
+                        </Col>
+                        <Col className="col pt-3 border-bottom d-flex">
+                            <p>Add profile in another language</p>
+                            <i className="fa fa-question-circle-o ml-auto"></i>
+                        </Col>
+                    </Row>
+                    <p>People also viewed</p>
+                    {this.state.users.map((user,i)=>{
+                        return (
+                            <Row className="pb-3" key={i}>
+                                <Col md={4}>
+                                    <img
+                                        src='https://img.icons8.com/officel/2x/user.png'
+                                        style={{height:"5rem", width: "5rem"}}
+                                        className="card-img img-fluid"
+                                        alt="image"
+                                    />
+                                    
+                                </Col>
+                                <Col className="col col-8 d-flex pt-3 border-bottom">
+                                    <Row className="w-100">
+                                        <Col sm={10} className="d-flex flex-column w-75">
+                                        <strong>{user.name} {user.surname}</strong>
+                                        <span>{user.title}</span>
+                                        </Col>
+                                        <Col sm={2}>
+                                        <span><i className="fa fa-user-plus"></i></span>
+                                        </Col>
+                                    </Row>                                        
+                                </Col>
+                            </Row>
+                        )
+                    })}
+                </Col>
         )
     }
     
