72ee7f65714787e5a3c190b22a9206246c519fe7
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-06-16 17:46:41 +0200

    second commit

--- linkedin_react/src/App.js
+++ linkedin_react/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"; 
-import NavBar from './components/NavBar';
+import React, { Component } from 'react';
+import Content from './components/Content'
+import { Container } from 'react-bootstrap'
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-<NavBar /> 
-    </div>
-  );
+  state = {
+    profiles: null,
+
+  }
+  componentDidMount = async () => {
+    await fetch("https://striveschool.herokuapp.com/api/profile/", {
+      headers: new Headers({
+        'Authorization': 'Basic ' + btoa("user16:c9WEUxMS294hN6fF"),
+        "Content-Type": "application/json",
+      }),
+    })
+      .then(response => response.json())
+      .then(respObj => this.setState({
+        profiles: respObj
+      }))
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Container fluid>
+          {console.log(this.state.profiles)}
+          {this.state.profiles && this.state.profiles.filter(profile => profile.name === "Klevin").map(profile => <Content key={profile._id} profileInfo={profile} />)}
+
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
