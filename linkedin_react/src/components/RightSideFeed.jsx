e74e1d50aac4d94e3eab353142395e3c7dcf6adf
segundara <segundara@gmail.com>
2020-06-17 15:55:54 +0300

    more content

--- linkedin_react/src/components/RightSideFeed.jsx
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -8,6 +8,7 @@ function RightSideFeed() {
                 <Card.Header className="d-flex justify-content-end">Ad...</Card.Header>
                 <Card.Body className="d-flex flex-column justify-content-between align-items-center">
                     <Card.Title>Get the latest jobs and industry news!</Card.Title>
+                    <Card.Img src="https://strive.school/hosted/images/4d/0e6c0c1c644c73a888e789e1be5c6e/flag.png"  style={{ width: '12rem', height: '12rem' }}></Card.Img>
                     <Card.Text>
                         Explore relevant opportunities with StriveSchool!
                     </Card.Text>
