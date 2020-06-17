c6578c394aa52e5757e2b586c9be7df5e7db3f85
segundara <segundara@gmail.com>
2020-06-17 16:49:36 +0300

    adjust the width and adding hr

--- linkedin_react/src/components/RightSideFeed.jsx
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -4,7 +4,7 @@ import {Col, Card, Button} from 'react-bootstrap'
 function RightSideFeed() {
     return (
         <Col md={3}>
-            <Card className="text-center" style={{ width: '30rem', height: '30rem' }}>
+            <Card className="text-center" style={{ width: '100%', height: '30rem' }}>
                 <Card.Header className="d-flex justify-content-end">Ad...</Card.Header>
                 <Card.Body className="d-flex flex-column justify-content-between align-items-center">
                     <Card.Title>Get the latest jobs and industry news!</Card.Title>
@@ -15,6 +15,7 @@ function RightSideFeed() {
                     <Button variant="outline-primary">Follow</Button>
                 </Card.Body>
             </Card>
+            <hr/>
         </Col>
     )
 }
