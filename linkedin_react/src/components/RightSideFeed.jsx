d852f13f7310054544db14c1f493ca4aaa0ae909
klevinb <klevin.bazaiti98@gmail.com>
2020-06-17 17:34:14 +0200

    sideBar fontsize change!

--- linkedin_react/src/components/RightSideFeed.jsx
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -4,9 +4,9 @@ import { FaAngleDown } from 'react-icons/fa'
 
 function RightSideFeed() {
     return (
-        <div style={{ fontSize: '18px' }}>
+        <div >
             <Card className="text-center" style={{ width: '100%', height: '35rem' }}>
-                <Card.Header className="d-flex justify-content-end">Ad...</Card.Header>
+                <Card.Header className="d-flex justify-content-center">Add to your feed</Card.Header>
                 <Card.Body className="d-flex flex-column justify-content-between align-items-center">
                     <Card.Title>Get the latest jobs and industry news!</Card.Title>
                     <Card.Img src="https://strive.school/hosted/images/4d/0e6c0c1c644c73a888e789e1be5c6e/flag.png" style={{ width: '12rem', height: '12rem' }}></Card.Img>
@@ -17,7 +17,7 @@ function RightSideFeed() {
                 </Card.Body>
             </Card>
             <hr></hr>
-            <div>
+            <div style={{ fontSize: '12px' }}>
                 <div className="d-flex justify-content-around">
                     <p>About</p>
                     <p>Accessibility</p>
