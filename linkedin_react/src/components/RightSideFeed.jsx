b4ff613c27123e361f82b01695563965a2a387a7
segundara <segundara@gmail.com>
2020-06-17 15:54:49 +0300

    adding content to the component

--- linkedin_react/src/components/RightSideFeed.jsx
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -4,7 +4,17 @@ import {Col, Card, Button} from 'react-bootstrap'
 function RightSideFeed() {
     return (
         <Col md={3}>
-            Some Content to the right!!!
+            <Card className="text-center" style={{ width: '30rem', height: '30rem' }}>
+                <Card.Header className="d-flex justify-content-end">Ad...</Card.Header>
+                <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+                    <Card.Title>Get the latest jobs and industry news!</Card.Title>
+                    <Card.Text>
+                        Explore relevant opportunities with StriveSchool!
+                    </Card.Text>
+                    <Button variant="outline-primary">Follow</Button>
+                </Card.Body>
+                <Card.Footer className="text-muted"></Card.Footer>
+            </Card>
         </Col>
     )
 }
