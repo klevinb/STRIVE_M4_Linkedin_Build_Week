eace5cfbe632462870b0b9ede30ae941a689d3cf
segundara <segundara@gmail.com>
2020-06-17 17:22:54 +0300

    adding footer to the right side of the feed

--- linkedin_react/src/components/RightSideFeed.jsx
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Col, Card, Button} from 'react-bootstrap'
+import {Col, Card, Button, Image} from 'react-bootstrap'
 
 function RightSideFeed() {
     return (
         <Col md={3}>
-            <Card className="text-center" style={{ width: '100%', height: '30rem' }}>
+            <Card className="text-center" style={{ width: '100%', height: '25rem' }}>
                 <Card.Header className="d-flex justify-content-end">Ad...</Card.Header>
                 <Card.Body className="d-flex flex-column justify-content-between align-items-center">
                     <Card.Title>Get the latest jobs and industry news!</Card.Title>
@@ -16,6 +16,29 @@ function RightSideFeed() {
                 </Card.Body>
             </Card>
             <hr/>
+            <div>
+                <div className="d-flex justify-content-around">
+                    <p>About</p>
+                    <p>Accessibility</p>
+                    <p>Help Center</p>
+                </div>
+                <div className="d-flex justify-content-around">
+                    <p>Privacy & Terms</p>
+                    <p>Ad Choices</p>
+                    <p>Advertising</p>
+                </div>
+                <div className="d-flex justify-content-around">
+                    <p>Business Services</p>
+                    <p>Get the LinkedIn app</p>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <p>More</p>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <Image src="/assets/linkedin-logo.png" style={{ width: '5rem', height: '1.5rem' }} />
+                    <span>LinkedIn Corporation &#169;2020</span>
+                </div>
+            </div>
         </Col>
     )
 }
