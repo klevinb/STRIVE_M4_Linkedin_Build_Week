c8c2d462cf750f43cb7abacc6fd3ece909c8e7f3
segundara <segundara@gmail.com>
2020-06-17 15:47:40 +0300

    adding right side component

---
+++ linkedin_react/src/components/RightSideFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Col, Card, Button} from 'react-bootstrap'
+
+function RightSideFeed() {
+    return (
+        <div>
+            Some Content to the right!!!
+        </div>
+    )
+}
+
+export default RightSideFeed
