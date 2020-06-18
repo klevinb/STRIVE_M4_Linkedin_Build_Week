15dc1c5f7968e25daaa4482207cea942d53e3198
segundara <segundara@gmail.com>
2020-06-18 17:22:44 +0300

    feed update

--- linkedin_react/src/components/FeedPosts.jsx
+++ linkedin_react/src/components/FeedPosts.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Button, DropdownButton, Dropdown } from 'react-bootstrap'
+import { Image, Button, DropdownButton, Dropdown, Modal, Form, Row, Col } from 'react-bootstrap'
 import { BsThreeDots } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
 import { AiOutlineLike, AiFillLike } from 'react-icons/ai'
@@ -10,7 +10,12 @@ class FeedPosts extends Component {
 
     state = {
         clicked: false,
-        showDropdown: false
+        showDropdown: false,
+        showModal: false,
+        editNewsFeed: false,
+        editPostId: this.props.info._id,
+        newsFeed: this.props.info.text,
+        image: ''
     }
 
     deletePost = (id) => {
@@ -20,8 +25,57 @@ class FeedPosts extends Component {
         });
     }
 
+    
+    handleChange = (event) => {
+        this.setState({
+            newsFeed: event.currentTarget.value
+        });
+
+    }
+
+    
+    saveImg = (event) => {
+        let photo = new FormData()
+        photo.append('post', event.target.files[0])
+        this.setState({
+            image: photo
+        });
+    }
+
+
+    
+    editPost = (e) => {
+        e.preventDefault()
+        const url = 'https://striveschool.herokuapp.com/api/posts/'+this.state.editPostId
+        Promise.all(
+            [
+                fetch(url, {
+                    method: "PUT",
+                    body: JSON.stringify(this.state.newsFeed),
+                    headers: new Headers({
+                        'Authorization': 'Basic ' + "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
+                        "Content-Type": "application/json",
+                    })
+                }),
+
+                fetch(url, {
+                    method: "POST",
+                    body: this.state.image,
+                    headers: new Headers({
+                        'Authorization': 'Basic ' + "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
+                    })
+                })
+            ]
+        ).then(this.setState({
+            showModal: false,
+            editNewsFeed: false,
+        }))
+            .catch(err => console.log(err))
+    }
+
     render() {
         return (
+            <>
             <div className="postContent box-shadow  mb-2">
                 <div className='postHeader d-flex align-items-center p-3'>
                     <div id="postImage" className='mr-3'>
@@ -53,7 +107,7 @@ class FeedPosts extends Component {
                     </div>
                     <div id="dropDownMenu">
                         <Dropdown.Menu show={this.state.showDropdown}>
-                            <Dropdown.Item onSelect={() => console.log("Edit")}>Edit</Dropdown.Item>
+                            <Dropdown.Item onSelect={() => this.setState({ showModal: true, editNewsFeed: true }) }>Edit</Dropdown.Item>
                             <Dropdown.Item onSelect={() => this.deletePost(this.props.info._id)}>Delete</Dropdown.Item>
                             <Dropdown.Item onSelect={() => console.log("Something else")}>Something else</Dropdown.Item>
                         </Dropdown.Menu>
@@ -85,6 +139,44 @@ class FeedPosts extends Component {
                     </div>
                 </div>
             </div >
+
+            <Modal show={this.state.showModal} onHide={() => this.setState({ showModal: false, editNewsFeed: false })}>
+            
+            {this.state.editNewsFeed &&
+                <>
+                    <Modal.Header >
+                        <Modal.Title>Edit this Post</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form className="d-flex flex-column" onSubmit={this.editPost}>
+                            <Form.Group controlId="feed">
+                                <Form.Label>Your Post Here</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    value={this.state.newsFeed}
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Group>
+
+                            <label>Image</label>
+                            <input type="file" id="image" profile="file" onChange={this.saveImg} accept="image/*" />
+                            <br></br>
+                            <div className="d-flex justify-content-center">
+                                <Button className="align-self-center mr-4" variant="warning" type="submit" onClick={() => console.log(this.props.info._id)}>
+                                    Edit
+                            </Button>
+                                <Button className="align-self-center" variant="danger" onClick={() => this.deletePost(this.state.editPostId)}>
+                                    Delete
+                            </Button>
+                            </div>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    </Modal.Footer>
+                </>
+            }
+            </Modal>
+            </>
 
         );
     }
