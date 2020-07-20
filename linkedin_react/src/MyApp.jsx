c7402b7aef716b5a6326a0b521c36c4b2f373c42
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-07-20 13:06:57 +0200

    login removed

--- linkedin_react/src/MyApp.jsx
+++ linkedin_react/src/MyApp.jsx
@@ -1,54 +1,46 @@
-import React, { Component } from 'react';
-import App from './App'
-import Login from './components/Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class MyApp extends Component {
+  state = {
+    authorizationKey: "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
+    username: "user16",
+    showApp: true,
+  };
 
-    state = {
-        authorizationKey: localStorage.getItem("authorizationKey"),
-        username: localStorage.getItem("username"),
-        showApp: true
-    }
+  getAuthorization = (autho, username) => {
+    localStorage.setItem("authorizationKey", autho);
+    localStorage.setItem("username", username);
+  };
+  resetAuthorization = () => {
+    localStorage.removeItem("authorizationKey");
+    localStorage.removeItem("username");
+    this.setState({ showApp: false, authorizationKey: "", username: "" });
+  };
 
-    getAuthorization = (autho, username) => {
-        localStorage.setItem("authorizationKey", autho)
-        localStorage.setItem("username", username)
+  showApp = () => {
+    this.setState({
+      authorizationKey: localStorage.getItem("authorizationKey"),
+      username: localStorage.getItem("username"),
+      showApp: true,
+    });
+  };
 
-    }
-    resetAuthorization = () => {
-        localStorage.removeItem("authorizationKey")
-        localStorage.removeItem("username")
-        this.setState({ showApp: false, authorizationKey: '', username: '' });
-    }
-
-    showApp = () => {
-        this.setState({ authorizationKey: localStorage.getItem("authorizationKey"), username: localStorage.getItem("username"), showApp: true });
-    }
-
-
-
-    render() {
-        return (
-            <>
-                <Router>
-                    {this.state.showApp && this.state.authorizationKey !== null &&
-                        <App
-                            authoKey={this.state.authorizationKey}
-                            username={this.state.username}
-                        />
-                    }
-                    <Route path="/" exact render={(props) =>
-                        <Login
-                            {...props}
-                            getAuthorization={this.getAuthorization}
-                            showApp={this.showApp}
-                            resetAuthorization={this.resetAuthorization} />}
-                    />
-                </Router>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Router>
+          {this.state.showApp && this.state.authorizationKey !== null && (
+            <App
+              authoKey={this.state.authorizationKey}
+              username={this.state.username}
+            />
+          )}
+        </Router>
+      </>
+    );
+  }
 }
 
 export default MyApp;
